6fa39077cc0068b88cb39eaf153005bc3035530f
Brent Vale <brentvale@gmail.com>
2017-07-24 13:42:40 -0700

    updates database settings to use pymysql

--- heartapp/heartapp/settings.py
+++ heartapp/heartapp/settings.py
@@ -74,9 +74,11 @@ WSGI_APPLICATION = 'heartapp.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': { 
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'django_db',                     
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
